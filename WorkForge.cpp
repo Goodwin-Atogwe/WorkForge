82e04dc15e7946ea8a3b17e0f334d7903278a032
Belgian Tiger <anesiatogwe@gmail.com>
2025-04-06 16:33:20 +0200

    added drawOption

--- WorkForge.cpp
+++ WorkForge.cpp
@@ -3,12 +3,19 @@
 void drawHeader(const char* hName) {
     std::cout << "------------------------------------------------------------\n";
     std::cout << "------------------------------------------------------------\n";
-    std::cout << hName ;
+    std::cout << "\t\t" << hName;
     std::cout << "\n------------------------------------------------------------\n";
     std::cout << "------------------------------------------------------------\n";
  }
 
+void drawOption(const char* oName) {
+    std::cout << oName << "\n";
+}
+
 int main()
 {
     drawHeader("EMPLOYEE MANAGEMENT SYSTEM");
+    drawOption("1. Login as Admin");
+    drawOption("2. Login as Manager");
+    drawOption("3. Exit");
 }
